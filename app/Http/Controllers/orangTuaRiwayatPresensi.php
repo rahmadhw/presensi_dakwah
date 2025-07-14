804633e2cb932a76255f4f1238a2d929acae02af
rahmadhw <rahmadhidyat098@gmail.com>
2025-07-14 23:10:34 +0700

    add grafik

--- app/Http/Controllers/orangTuaRiwayatPresensi.php
+++ app/Http/Controllers/orangTuaRiwayatPresensi.php
@@ -7,21 +7,24 @@ use App\Models\Kelas;
 use App\Models\mataPelajaran;
 use App\Models\Siswa;
 use App\Models\tahunAjaran;
+use App\Models\OrangTua;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class orangTuaRiwayatPresensi extends Controller
 {
     public function index(Request $request)
     {
-       $absensi = Absensi::with('siswa')
-            ->whereHas('siswa', function ($query) {
-                $query->where('orang_tua_id', Auth::user()->id);
+
+        $orangTua  = OrangTua::where('user_id', Auth::user()->id)->first();
+       $data = Absensi::with('siswa')
+            ->whereHas('siswa', function ($query) use ($orangTua) {
+                $query->where('orang_tua_id', $orangTua->id);
             })
             ->get();
 
-        dd($absensi);
 
-        return view('orang_tua.riwayat_presensi.index', compact('kelas', 'selectedKelas', 'tanggal', 'data'));
+        return view('orang_tua.riwayat_presensi.index', compact('data'));
     }
 
 
